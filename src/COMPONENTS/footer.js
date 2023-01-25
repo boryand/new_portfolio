7b5a511ae82adfae39158cdd71eb973138073632
boryand <boryand>
2023-01-25 13:18:11 +0100

    The final touches are done , and finally the portfolio is readyy

--- src/COMPONENTS/footer.js
+++ src/COMPONENTS/footer.js
@@ -3,11 +3,8 @@
 const Footer = () => {
   return (
     <div className="footer_container">
-      <p className="build_with_love"> Build with ❤️ from Boryana Dimitrova</p>
-      <div className="footer_container2">
-        {/* <img className="mail_to_me" src={email}></img>
-        <img className="linked" src={linkedin}></img> */}
-      </div>
+      <p className="build_with_love"> Build with 💙 from Boryana Dimitrova ©</p>
+    
       <p className="build_with_love"> 2022</p>
      
     </div>
